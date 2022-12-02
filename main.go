86f8548e8079ccdaa50e23a7a1d4cada7ef6a64b
Sean Banko <sean.banko@gmail.com>
2022-12-02 12:48:50 -0500

    feat: run program in alt screen quit with q, ctrl+c

--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
-    tea "github.com/charmbracelet/bubbletea"
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 type model struct{}
 
 func main() {
-    tea.NewProgram(model{}) 
+	p := tea.NewProgram(model{}, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initialModel() model {
+	return model{}
 }
 
 func (m model) Init() tea.Cmd {
-    return nil
+	return nil
 }
 
-func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
-    return m, nil
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
 }
 
 func (m model) View() string {
-    return ""
+	return ""
 }
