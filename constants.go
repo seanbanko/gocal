f1ba044335be2abb8d40123851779452c249bd7c
Sean Banko <sean.banko@gmail.com>
2022-12-23 17:33:47 -0500

    refactor: move global constants to constants.go

---
+++ constants.go
@@ -0,0 +1,14 @@
+package main
+
+const (
+	YYYYMMDD            = "2006-01-02"
+	MMDDYYYY            = "01/02/2006"
+	HHMMSS24h           = "15:04:05"
+	HHMM24h             = "15:04"
+	HHMMSS12h           = "3:04:05 PM"
+	HHMM12h             = "3:04 PM"
+	MMDDYYYYHHMM24h     = "01/02/2006 15:04"
+	TextDate            = "January 2, 2006"
+	TextDateWithWeekday = "Monday, January 2, 2006"
+	AbbreviatedTextDate = "Jan 2 Mon"
+)
